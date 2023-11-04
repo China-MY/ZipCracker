6e6057aee770f70dbf26099769441d83b4296443
China-MY <mingyuxuezhang@qq.com>
2023-11-04 20:55:53 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,59 @@
+本人只是在大佬的工具上进行了二次开发
+
+还望指正
+
+
+
+### 工具简介
+
+---
+
+ZipCracker是一款由Hx0战队开发的高性能多并发破解工具，专为破解密码保护的Zip文件而设计。它采用CRC32碰撞和字典攻击方式猜测Zip文件的明文或密码，并能成功提取其中的内容。这款工具具备识别"伪加密"Zip文件的能力，并能自动进行修复。因此，它非常适合在CTF比赛中使用。
+
+PS：
+1.程序会自动检查加密压缩包中的文件大小，若小于6字节，会主动询问用户是否使用CRC32进行Hash碰撞。
+
+2.程序自带6000个常用的爆破字典，同时还会生成0-6位的纯数字字典。
+
+### 使用方法
+
+---
+
+#### 1.伪加密识别及修复
+```
+python3 ZipCracker.py test01.zip
+```
+<img width="800" alt="image" src="https://github.com/asaotomo/ZipCracker/assets/67818638/88fd42b5-7b89-452d-a640-77326dc05b4c">
+
+#### 2.暴力破解-内置字典
+```
+python3 ZipCracker.py test02.zip
+```
+<img width="800" alt="image" src="https://github.com/asaotomo/ZipCracker/assets/67818638/bcfdc434-3eb2-426f-8f83-7951c9af4b59">
+
+#### 3.暴力破解-用户自定义字典
+```
+python3 ZipCracker.py test02.zip MyDict.txt
+```
+<img width="800" alt="image" src="https://github.com/asaotomo/ZipCracker/assets/67818638/c11fd091-b4a5-4b5f-ab39-8489604cf57d">
+
+#### 4.暴力破解-CRC32碰撞
+```
+python3 ZipCracker.py test03.zip
+```
+<img width="800" alt="image" src="https://github.com/asaotomo/ZipCracker/assets/67818638/db48ca5c-3c24-44a6-8a79-0398b1b56222">
+
+---
+
+**本工具仅提供给安全测试人员进行安全自查使用**，**用户滥用造成的一切后果与作者无关**，**使用者请务必遵守当地法律** **本程序不得用于商业用途，仅限学习交流。**
+
+---
+
+**扫描关注战队公众号，获取最新动态**
+
+![image](https://github.com/asaotomo/ZipCracker/assets/67818638/76fd25ca-a098-41bc-968e-c758b2848c73)
+
+**【知识星球】福利大放送**
+
+![image](https://github.com/asaotomo/ZipCracker/assets/67818638/acb99c55-9fcb-4e1f-9258-26b313cb81e2)
+
---
+++ ZipCracker.py
@@ -0,0 +1,187 @@
+import os
+import shutil
+import sys
+from threading import Thread
+from collections import OrderedDict
+import time
+import zipfile
+import binascii
+import string
+from itertools import product
+import itertools as its
+
+
+def is_zip_encrypted(file_path):
+    """
+    检查zip文件是否存在伪加密
+    """
+    with zipfile.ZipFile(file_path) as zf:
+        for info in zf.infolist():
+            if info.flag_bits & 0x1:
+                return True
+    return False
+
+
+def fix_zip_encrypted(file_path):
+    """
+    修复伪加密的zip文件
+    """
+    temp_path = file_path + ".tmp"
+    with zipfile.ZipFile(file_path) as zf, zipfile.ZipFile(temp_path, "w") as temp_zf:
+        for info in zf.infolist():
+            if info.flag_bits & 0x1:
+                info.flag_bits ^= 0x1  # 清除加密标记
+            temp_zf.writestr(info, zf.read(info.filename))
+    fix_zip_name = "fix_" + file_path
+    try:
+        shutil.move(temp_path, fix_zip_name)
+    except:
+        os.remove(fix_zip_name)
+        shutil.move(temp_path, fix_zip_name)
+    return fix_zip_name
+
+
+def crack_password(password, stop):
+    global success, cost_time
+    try:
+        zf = zipfile.ZipFile(zip_file)
+        zf.setpassword(password.encode())
+        zf.extractall()
+        print(f'\n[*]恭喜您！密码破解成功,该压缩包的密码为：{password}')
+        success = True
+        filenames = zf.namelist()
+        print(f"[*]系统已为您自动提取出{len(filenames)}个文件：{filenames}")
+        stop[0] = True  # 破解成功，设置 stop 为 True
+        os._exit(0)
+        return True
+    except:
+        passwords_cracked = len(tried_passwords)
+        avg_cracked = int(passwords_cracked / cost_time)
+        remaining_time = time.strftime('%H:%M:%S',
+                                       time.gmtime((total_passwords - passwords_cracked) / avg_cracked))
+        print("\r[-]当前破解进度：{:.2f}%，剩余时间：{}，当前时速：{}个/s，正在尝试密码:{:<20}".format(
+            passwords_cracked / total_passwords * 100, remaining_time, avg_cracked, password),
+            end="", flush=True)
+    finally:
+        cost_time += time.time() - start
+    return False
+
+
+def get_crc(zip_file, fz):
+    key = 0
+    for filename in fz.namelist():
+        if filename.endswith('/'):  # skip directories
+            continue
+        getSize = fz.getinfo(filename).file_size
+        if getSize <= 6:
+            sw = input(
+                f'[!]系统监测到压缩包 {zip_file} 中的 {filename} 文件大小为{getSize}字节，是否尝试通过CRC32碰撞的方式直接爆破该文件内容？（y/n）')
+            if sw == 'y' or sw == "Y":
+                getCrc = fz.getinfo(filename).CRC
+                print(f'[+]{filename} 文件的CRC值为：{getCrc}')
+                crack_crc(filename, getCrc, getSize)
+                key += 1
+    if key >= len([name for name in fz.namelist() if not name.endswith('/')]):  # only count files, not directories
+        print(f'[*]系统检测到 {zip_file} 中的所有文件均已通过CRC32碰撞破解完成，将不再使用字典进行暴力破解！')
+        exit()
+    else:
+        pass
+
+
+def crack_crc(filename, crc, size):
+    dic = its.product(string.printable, repeat=size)
+    print(f"[+]系统开始进行CRC32碰撞破解······")
+    for s in dic:
+        s = ''.join(s).encode()
+        if crc == (binascii.crc32(s)):
+            print(f'[*]恭喜您，破解成功！\n[*]{filename} 文件的内容为：' + str(s.decode()))
+            break
+
+
+if __name__ == '__main__':
+    print("""                          
+ ______          ____                _   [*]Hx0战队      
+|__  (_)_ __    / ___|_ __ __ _  ___| | _____ _ __ 
+  / /| | '_ \  | |   | '__/ _` |/ __| |/ / _ \ '__|
+ / /_| | |_) | | |___| | | (_| | (__|   <  __/ |   
+/____|_| .__/___\____|_|  \__,_|\___|_|\_\___|_|   
+       |_| |_____|                                 
+#Coded By Asaotomo               Update:2023.09.25
+        """)
+    if len(sys.argv) == 1:
+        print(
+            "[*]用法1(内置字典):Python3 Hx0_Zip_Cracker.py YourZipFile.zip \n[*]用法2(自定义字典):Python3 Hx0_Zip_Cracker.py  YourZipFile.zip  YourDict.txt")
+        os._exit(0)
+    zip_file = sys.argv[1]
+    if is_zip_encrypted(zip_file):
+        print(f'[!]系统检测到 {zip_file} 是一个加密的ZIP文件')
+        zf = zipfile.ZipFile(zip_file)
+        try:
+            fix_zip_name = fix_zip_encrypted(zip_file)
+            zf = zipfile.ZipFile(fix_zip_name)
+            zf.testzip()
+            zf.extractall()
+            filenames = zf.namelist()
+            print(
+                f"[*]压缩包 {zip_file} 为伪加密，系统已为您生成修复后的压缩包({fix_zip_name})，并自动提取出{len(filenames)}个文件：{filenames}")
+            os._exit(0)
+        except Exception as e:
+            zf = zipfile.ZipFile(zip_file)
+            print(f'[+]压缩包 {zip_file} 不是伪加密，准备尝试暴力破解')
+            # crc32碰撞
+            get_crc(zip_file, zf)
+            # 破解加密的zip文件
+            password_list = []
+            if len(sys.argv) > 2:  # 检查是否指定了自定义字典文件
+                dict_file = sys.argv[2]
+                dict_type = "用户自定义字典"
+            else:
+                dict_file = 'password_list.txt'
+                dict_type = "系统内置字典"
+            with open('password_list.txt', 'r') as f:
+                password_list += [line.strip() for line in f.readlines()]
+            print(f'[+]加载{dict_type}成功！')
+            # for length in range(1, 7):
+            #     password_list += [f'{i:0{length}d}' for i in range(10 ** length)]
+            # print(f'[+]加载0-6位纯数字字典成功！')
+            print("生成字典会卡顿请耐心等待！！！")
+            length = int(input("请输入密码长度: "))
+            is_numeric = input("密码是否仅包含数字？(是1/否0): ").lower() == "1"
+            is_lowercase = input("密码是否包含小写字母？(是1/否0): ").lower() == "1"
+            is_uppercase = input("密码是否包含大写字母？(是1/否0): ").lower() == "1"
+            is_special = input("密码是否包含特殊字符？(是1/否0): ").lower() == "1"
+            characters = ''
+
+            if is_numeric:
+                characters += string.digits
+            if is_lowercase:
+                characters += string.ascii_lowercase
+            if is_special:
+                characters += string.punctuation
+            if is_uppercase:
+                characters += string.ascii_uppercase
+
+            password_list = [''.join(p) for p in product(characters, repeat=length)]
+            print(
+                f'[+] 加载{length}位{"数字" if is_numeric else "数字和字母"}{"和小写字母" if is_lowercase else ""}{"和特殊字符" if is_special else ""}{"和大写字母" if is_uppercase else ""}字典成功！')
+
+            password_list = list(OrderedDict.fromkeys(password_list))
+            tried_passwords = []
+            total_passwords = len(password_list)
+            print(f"[+]当前爆破字典总数:{total_passwords}个")
+            print(f"[+]系统开始进行暴力破解······")
+            success = False
+            cost_time = 0.00001
+            stop = [False]  # 用列表存储 stop 变量，使其可以在多个线程间共享
+            threads = []
+            for password in password_list:
+                tried_passwords.append(password)
+                if not stop[0]:  # 检查 stop 变量的值，决定是否启动线程
+                    start = time.time()
+                    t = Thread(target=crack_password, args=[password, stop])
+                    threads.append(t)
+                    t.start()
+            for t in threads:
+                t.join()
+            if not success:
+                print('\n[-]非常抱歉，字典中的所有密码均已尝试，请尝试其他字典或使用更高级的破解方法！')
